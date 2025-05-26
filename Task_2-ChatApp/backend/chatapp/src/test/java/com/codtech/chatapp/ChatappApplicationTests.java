f67cf18d37f30a534a6350c0124d6a1e415d03f2
Keerthika <keerthikakrish2004@gmail.com>
2025-05-26 16:53:43 +0530

    Task 2: ChatApp backend setup finished

---
+++ Task_2-ChatApp/backend/chatapp/src/test/java/com/codtech/chatapp/ChatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codtech.chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
